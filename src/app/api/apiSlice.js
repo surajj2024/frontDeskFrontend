92de637ad05e98c48cd4f1b896175f24afa08ce3
surajj2024 <surajjkumar2024@gmail.com>
2025-01-08 17:13:19 +0530

    fix: :bug: update the baseUrl to render url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    // baseUrl: 'https://clinic-api-test.onrender.com/',
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://frontdeskclinic.onrender.com/',
+    // baseUrl: 'http://localhost:3500',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
